03b29774f11d43e482bfb2026bd1c8b94f0cbc15
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-22 09:31:03 +0530

    first commit

---
+++ greatest_three.py
@@ -0,0 +1,9 @@
+a,b,c = int(input("enter the first value:")),int(input("enter the  second value:")),int(input("enter the third value:"))
+
+
+if ((a>b) and (a>c)):
+    print("a is biggest:",a)
+elif (b>c):
+    print("b is biggest:",b)
+else:
+    print("c is biggest:",c)
